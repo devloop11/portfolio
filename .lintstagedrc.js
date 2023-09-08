8b8ca6a47a91486850404e1cafb076351d38709e
devloop11 <dev@looplava.com>
2023-09-08 20:07:15 +0300

    with lint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,7 @@
-module.exports = {
-    '*.{js,jsx,ts,tsx,css,scss,json,md}': ['eslint --fix', 'prettier --write --ignore-unknown', 'git add'],
+export default {
+  "*.{js,jsx,ts,tsx,css,scss,json,md}": [
+    "eslint --fix",
+    "prettier --write --ignore-unknown",
+    "git add",
+  ],
 };
